47870dbebf5ea0d1a1544a25ac9fc5c9c995c6bc
Wivel <wiveloriginal@yandex.ru>
2018-10-20 22:10:20 +0300

    have mistake in func =\

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ function addNull(num) {
     }
     return str;
 }
+
+function getBirthday(elem) {
+    let
+        Arr         = String(elem.value).split('-'),
+        BirtDate    = new Date(Arr[0], String((Number(Arr[1])-1)), Arr[2]),
+        Now         = new Date(),
+        MouthNow    = Now.getMonth(),
+        ChangedYear = Now.getFullYear();
+    if (MouthNow > Arr[1]) {
+        ChangedYear++;
+    }
+    else {
+        if (MouthNow == Arr[1] && Arr[2] > Now.getDate()) {
+            ChangedYear++;
+        }
+    }
+    let
+        Needdate = new Date(String(ChangedYear),String((Number(Arr[1])-1)),Arr[2]),
+        NeeddateSec = Needdate.getTime(),
+        NowSec      = Now.getTime(),
+        dif         = (NeeddateSec - NowSec)/(1000*60*60*24);
+    alert(dif.toFixed())
+
+
+}
